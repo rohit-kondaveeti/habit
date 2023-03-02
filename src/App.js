2dd9bec5d0927c8f84d81ad10a85b7cc67ee9c56
Rohit <112994888+rohit-kondaveeti@users.noreply.github.com>
2023-03-03 00:21:17 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <BrowserRouter >
+      <BrowserRouter basename={"/habit_tracker"} >
         <Routes>
           <Route path="/" element={<Home />}></Route>
           <Route path="/habit/:id" element={<Habits />}></Route>
