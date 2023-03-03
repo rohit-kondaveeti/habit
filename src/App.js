481a6de77e1ca7b0f69e6ef0a94498978d0db6d2
Rohit <112994888+rohit-kondaveeti@users.noreply.github.com>
2023-03-03 08:25:22 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+import Habits from "./Habits";
+import Home from "./Home";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter basename={"/"}>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/habit/:id" element={<Habits />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
